5827014aa4faa68a9595c4abcb5dae58e21ba54d
Henry Nguyen <h.nguyen-4@student.tudelft.nl>
2025-01-14 00:01:20 +0100

    a

--- map/grass_tile.gd
+++ map/grass_tile.gd
@@ -7,6 +7,24 @@ var pref: PathwayPreference = null
 func setPref(preference: PathwayPreference) -> void:
 	pref = preference
 
+var is_mouse_inside = false
+
+func _ready():
+	$"DescriptionPanel/DescriptionText".text = ""
+	$TowerSlots.hovered.connect(_on_hovered)
+	$TowerSlots.unhovered.connect(_on_unhovered)
+	
+			
+func _on_hovered():
+	if pref:
+		$DescriptionPanel.visible=true
+		$"DescriptionPanel/DescriptionText".text = pref.description
+		print(pref.description)
+
+func _on_unhovered():
+	$DescriptionPanel.visible=false
+	
+
 func pathwayPreferenceFulfilled() -> bool:
 	if pref == null:
 		return true
